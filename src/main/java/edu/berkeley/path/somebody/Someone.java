0525063c526ba07ac54802aea6a8e082da318f0f
Brian Peterson <brian@brians-mbp-2.dyn.berkeley.edu>
2017-10-04 15:30:29 -0700

    Initial commit

---
+++ src/main/java/edu/berkeley/path/somebody/Someone.java
@@ -0,0 +1,15 @@
+package edu.berkeley.path.somebody;
+
+/**
+ * Created by brian on 9/29/17.
+ */
+public class Someone {
+
+    private String target = "brian";
+
+    public String getTarget(){
+        return target;
+    }
+
+
+}
